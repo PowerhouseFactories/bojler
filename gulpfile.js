34f0a27e61e236217e3d91c655198c4e29721dfe
Dave Werner <werner@dwernermacpro3.empowermm.com>
2018-01-24 11:06:36 -0500

    Reverting to the state of the project at d775fb9

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,149 @@
-var gulp = require( 'gulp' );
+const gulp = require( 'gulp' ),
+		sass = require( 'gulp-sass' ),
+		gcmq = require( 'gulp-group-css-media-queries' ),
+		gutil = require( 'gulp-util' ),
+		juice = require( '@akzhan/gulp-juice' ),
+		del = require( 'del' ),
+		stripComments = require( 'gulp-strip-comments' ),
+		connect = require( 'gulp-connect' ),
+		path = require( 'path' ),
+		filesToSass = [
+			'source/sass/inlined.scss',
+			'source/sass/embedded.scss',
+		],
+		filesToWatch = [
+			'source/sass/**/*.scss',
+			'source/**/*.html',
+		];
 
-// Require main configuration file
-var config = require( './gulp/config.js' );
+// Build SASS
+gulp.task( 'build:sass', function( done ) {
+	'use strict';
 
-// Require tasks
-var sass = require( './gulp/tasks/sass.js' );
-var test = require( './gulp/tasks/test.js' );
-var examples = require( './gulp/tasks/examples.js' );
-var release = require( './gulp/tasks/release.js' );
+	return gulp.src( filesToSass )
+		.pipe(
+			sass( {
+				outputStyle: 'compressed',
+			} )
+			.on( 'error', gutil.log )
+		)
+		.pipe( gcmq() )
+		.pipe( gulp.dest( 'public/css/' ) )
+		.on( 'end', done );
+} );
 
-// SASS build task
+// Inline CSS
+gulp.task( 'inline:css', function( done ) {
+	'use strict';
+
+	return gulp.src( 'source/**/*.html' )
+		.pipe(
+			juice( {
+				applyHeightAttributes: false,
+				applyWidthAttributes: false,
+				xmlMode: true,
+				webResources: {
+					relativeTo: path.resolve( __dirname, 'public/' ),
+					images: false,
+					svgs: false,
+					scripts: false,
+					links: false,
+				},
+			} )
+			.on( 'error', gutil.log )
+		)
+		.pipe( gulp.dest( 'public/' ) )
+		.on( 'end', done );
+} );
+
+// Clean CSS
+gulp.task( 'clean:css', function( done ) {
+	'use strict';
+
+	return del( [
+		'public/css/',
+	] )
+	.then( function() {
+		done();
+	} );
+} );
+
+// Clean HTML
+gulp.task( 'clean:html', function( done ) {
+	'use strict';
+
+	return gulp.src( 'public/**/*.html' )
+		.pipe(
+			stripComments( {
+				safe: true,
+				trim: true,
+			} )
+			.on( 'error', gutil.log )
+		)
+		.pipe( gulp.dest( 'public/' ) )
+		.pipe( connect.reload() )
+		.on( 'end', done );
+} );
+
+// Default (Build)
 gulp.task(
-	'sass',
+	'default',
 	gulp.series( [
-		sass.lint,
-		sass.build,
+		'build:sass',
+		'inline:css',
+		'clean:css',
+		'clean:html',
 	] )
 );
 
-// Test build task
+// Start server w/ live reload
+gulp.task( 'start', function( done ) {
+	'use strict';
+
+	connect.server( {
+		port: 8000,
+		root: 'public',
+		livereload: true,
+	} );
+
+	done();
+} );
+
+// Watch
+gulp.task( 'watch', function( done ) {
+	'use strict';
+
+	gulp.watch(
+		filesToWatch,
+		gulp.series( [
+			'default',
+		] )
+	);
+
+	done();
+} );
+
+// Development mode
 gulp.task(
-	'test',
+	'dev',
 	gulp.series( [
-		test.clean,
-		test.build,
+		'default',
+		gulp.parallel( [
+			'start',
+			'watch',
+		] )
 	] )
 );
 
-// Test watch task
-gulp.task(
-	'test:watch',
-	function() {
-		gulp.watch(
-			config.paths.test.watch,
-			gulp.series( [
-				'sass',
-				'test',
-			] )
-		);
-	}
-);
+// Clean project folder
+gulp.task( 'clean', function( done ) {
+	'use strict';
 
-// Examples build and migration task
-gulp.task(
-	'examples',
-	gulp.series( [
-		'test',
-		examples.clean,
-		examples.move,
+	return del( [
+		'public/*',
+		'!public/assets',
 	] )
-);
-
-// Release main task
-gulp.task(
-	'release',
-	gulp.series( [
-		release.prompt,
-		release.version,
-		'sass',
-		'examples',
-		release.assets,
-	] )
-);
+	.then( function() {
+		done();
+	} );
+} );
